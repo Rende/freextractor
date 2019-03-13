9e10f84ba05ba1f4568131915dccd072e3773a41
Aydan Rende <aydan.rende@dfki.de>
2019-03-13 14:06:12 +0100

    Add text for ElasticsearchService

---
+++ src/test/java/de/dfki/mlt/freextractor/ElasticsearchServiceTest.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package de.dfki.mlt.freextractor;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+import org.junit.Test;
+
+import de.dfki.mlt.freextractor.flink.Entity;
+
+/**
+ * @author Aydan Rende, DFKI
+ *
+ */
+public class ElasticsearchServiceTest {
+
+	public ElasticsearchService esService = new ElasticsearchService();
+
+	@Test
+	public void testGetMultiEntities() {
+		List<String> idList = new ArrayList<String>();
+		idList.add("Q83030");
+		List<Entity> results = esService.getMultiEntities(idList);
+		List<HashMap<String, String>> actualClaims = results.get(0).getClaims();
+
+		String[] expectedProperties = { "P1343", "P2176", "P279", "P1995", "P910", "P31" };
+		List<String> expectedPropList = Arrays.asList(expectedProperties);
+		String[] expectedItems = { "Q2657718", "Q409672", "Q3065932", "Q7867", "Q8372268", "Q12136" };
+		List<String> expectedItemList = Arrays.asList(expectedItems);
+		for (HashMap<String, String> claim : actualClaims) {
+			if (claim.containsKey("property-id") && claim.containsKey("wikibase-item")) {
+				assert (expectedPropList.contains(claim.get("property-id")));
+				assert (expectedItemList.contains(claim.get("wikibase-item")));
+			}
+		}
+
+	}
+
+}
